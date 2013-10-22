2a34b6495b96fe52c3c781f91b247c435b94cfd3
kyle.mcconnaughay <miniweps@gmail.com>
2013-10-22 12:24:55 -0400

    Worked on infrastructure for the website

--- DocumentationWebsite/app.py
+++ DocumentationWebsite/app.py
@@ -8,5 +8,20 @@ app.debug = True
 def index():
 	return render_template("index.html")
 
+@app.route("/design")
+def design():
+	return render_template("design.html")
+
+@app.route("/timeline")
+def timeline():
+	return render_template("timeline.html")
+
+@app.route("/team")
+def team():
+	json_team_data = open("data/team.json")
+	team_data = json.load(json_team_data)
+	json_team_data.close()
+	return render_template("team.html", team=team_data)
+
 if __name__ == "__main__":
 	app.run()
